766a779ef8e529d7ae4f503e2f4050e7bc3b5b01
MustaphaMouchtarik <110406056+MustaphaMouchtarik@users.noreply.github.com>
2026-09-11 15:38:22 +0100

    LC-unique-3-digit-even-numbers [Runtime: N/A; Memory: N/A]

--- hash-table/unique-3-digit-even-numbers.py
+++ hash-table/unique-3-digit-even-numbers.py
@@ -1,9 +1,8 @@
 class Solution:
     def totalNumbers(self, digits: List[int]) -> int:
-        l=len(digits)
         d=Counter(digits)
         ans=0
-        for i in range(10**(l-1),10**l,2):
+        for i in range(100,1000,2):
             valid=True
             can=Counter([int(digit) for digit in str(i)])
             for k in can:
